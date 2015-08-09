efde5baa9494cf541b5e84e9435a4602e237eb44
Jonathan Perkin <jonathan@perkin.org.uk>
2015-08-09 23:46:53 +0100

    Use AJAX to test raw version exists first.
    Improves parsing considerably, matching more possible paste URLs and
    avoiding false positives such as http://pastebin.com/settings.
    
    The Chrome developer window complains about "Synchronous XMLHttpRequest
    on the main thread is deprecated" but I don't know of another way to do
    this.
    
    Version 1.1 already!

--- background.js
+++ background.js
@@ -4,31 +4,61 @@
  */
 var urlcb = function (details)
 {
-	/* Thanks to https://gist.github.com/jlong/2428561 */
-	var parser = document.createElement('a');
-
-	/* Try to only match paste URLs.  Not foolproof. */
-	var validpaste = new RegExp('^[A-Za-z0-9]{8}$');
-
+	/*
+	 * Thanks to https://gist.github.com/jlong/2428561 for the easy way
+	 * to parse a URL.
+	 */
+	var parser = document.createElement("a");
 	parser.href = details.url;
-	var pasteid = parser.pathname.replace(/^\//, '');
 
-	if (validpaste.test(pasteid)) {
+	/*
+	 * Try to only match paste URLs.  First exclude any URLs which
+	 * obviously don't match.
+	 */
+	var validpaste = new RegExp("^[A-Za-z0-9]+$");
+	var pasteid = parser.pathname.replace(/^\//, "");
+
+	if (!(validpaste.test(pasteid)))
+		return;
+
+	/*
+	 * If that succeeded then perform an AJAX request to check the
+	 * proposed URL works via raw.php.  We need to test both that the
+	 * returned status is 200, but also that the URL didn't change as
+	 * XMLHttpRequest will automatically follow redirects.
+	 *
+	 * This avoids false positives such as http://pastebin.com/settings
+	 */
+	var xhr = new XMLHttpRequest();
+	var rawurl = "http://pastebin.com/raw.php?i=" + pasteid;
+
+	xhr.open("HEAD", rawurl, false);
+	xhr.send();
+
+	if (xhr.status === 200 && xhr.responseURL === rawurl ) {
+		/*
+		 * We're now reasonable confident that the URL is a valid
+		 * paste, so return the modified URL redirect.
+		 */
 		return {
-			redirectUrl: parser.protocol +
-				     parser.hostname +
-				     parser.port +
-				     "/raw.php?i=" + pasteid
+			redirectUrl: rawurl
 		};
 	}
 }
 
+/*
+ * Note that we don't bother handling HTTPS or preserving parser.protocol
+ * as pastebin redirects HTTPS to HTTP first anyway (as of August 2015).
+ */
 var filter = {
-	urls : ["*://pastebin.com/*"]
+	urls : ["http://pastebin.com/*"]
 };
 
 var extraspec = [
 	"blocking"
 ];
 
+/*
+ * See https://developer.chrome.com/extensions/webRequest
+ */
 chrome.webRequest.onBeforeRequest.addListener(urlcb, filter, extraspec);
